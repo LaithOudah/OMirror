e592eca718f3131ab816369fad773415cf8cdf80
omar-hindawi98 <hindawi@student.chalmers.se>
2018-10-15 01:23:06 +0200

    Code for News written

--- news.py
+++ news.py
@@ -1,13 +1,39 @@
 import feedparser
 
 # rss link
-news_rss =
+news_rss = "http://www.aftonbladet.se/nyheter/rss.xml"
+
+news_max = 5
 
 updated = False
 
+newsArray = {}
+
 def news_Parse():
+    global updated, newsArray
+    newsParser = feedparser.parse(news_rss)
     
+    i=0
+    for post in newsParser.entries:
+        # save to news field
+        try:
+            if newsArray[i][0] != post.title:
+                updated = True
+        except Exception:
+            updated = False
+        newsArray[i] = (post.title, post.published)
+        
+        # Break
+        i += 1
+        if i == news_max:
+            break
 
 def newsUpdated():
-    return False
+    if updated:
+        updated = False
+        return True
+    else:
+        return False
 
+news_Parse()
+news_Parse()
